466f664ef909720122819abd7175db0ce48e2535
Matthew-Collins <matthew.collins@cwgsy.net>
2017-07-13 20:35:18 +0100

    Template MonoGame Template in VB.Net

---
+++ MyMonoGameVB/Main.vb
@@ -0,0 +1,6 @@
+﻿Public Class Main
+    Public Shared Sub Main()
+        Dim Game As New MyGame
+        Game.Run()
+    End Sub
+End Class
---
+++ MyMonoGameVB/MyGame.vb
@@ -0,0 +1,34 @@
+﻿Imports Microsoft.Xna.Framework
+Imports Microsoft.Xna.Framework.Graphics
+Imports Microsoft.Xna.Framework.Input
+
+Public Class MyGame
+    Inherits Game
+
+    Private Graphics As New GraphicsDeviceManager(Me)
+    Private SpriteBatch As SpriteBatch
+
+    Public Sub New()
+    End Sub
+
+    Protected Overrides Sub Initialize()
+        MyBase.Initialize()
+    End Sub
+
+    Protected Overrides Sub LoadContent()
+        Me.SpriteBatch = New SpriteBatch(Me.GraphicsDevice)
+    End Sub
+
+    Protected Overrides Sub UnloadContent()
+    End Sub
+
+    Protected Overrides Sub Update(gameTime As GameTime)
+        MyBase.Update(gameTime)
+    End Sub
+
+    Protected Overrides Sub Draw(gameTime As GameTime)
+        Me.GraphicsDevice.Clear(Color.CornflowerBlue)
+        MyBase.Draw(gameTime)
+    End Sub
+
+End Class
